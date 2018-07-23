5373578c3b0405ce8d0097c1daba7f27e62b71c3
sreejithsankar55 <32271222+sreejithsankar55@users.noreply.github.com>
2018-07-23 21:23:45 +0530

    Create ENCMSG.cpp
    Encoding message : Manipulation with strings

---
+++ ENCMSG.cpp
@@ -0,0 +1,62 @@
+/* (: Sreejith Codes :) */
+ 
+#include    <bits/stdc++.h>
+ 
+#define 	ll 	long long
+#define 	ld 	long double
+#define 	pb 	push_back
+#define 	ff 	first
+#define 	ss 	second
+#define 	mp 	make_pair
+#define 	mod  1000000007
+#define 	pi 	3.141592653589793
+ 
+using namespace std;
+ 
+int main()
+{
+		ll t,len;
+		cin>>t;
+		
+		
+		while(t--)
+		{
+			 string s;
+			 cin>>len;
+			 
+			 ll g;
+			 cin>>s;
+			 if(len%2==0)
+			 for ( ll i = 0 ; i < len ; i+=2 )
+			 {
+				 swap(s[i],s[i+1]);
+			 }
+			 else
+			 for ( ll i = 0 ; i < len-1 ; i+=2 )
+			 {
+				 swap(s[i],s[i+1]);
+			 }
+			 for ( ll i = 0 ; i < len ; i++ )
+			 {
+				 if(s[i]<=109){
+				 g=(int)(s[i])-97;
+				 s[i]=(122-g);
+				 }
+				 else
+				 {
+					g=122-(int)(s[i]); 
+				   s[i]=(97+g);
+					}
+				 cout<<s[i];
+			 }
+			 cout<<endl;
+			
+			
+			
+		}
+ 
+ 
+        return 0;
+}
+ 
+ 
